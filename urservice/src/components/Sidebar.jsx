ec8d48959403480cbc5f070275235c2785ef6d29
Janna Mae Bonior <maebonior22@gmail.com>
2022-10-22 18:07:27 +0800

    Update

--- urservice/src/components/Sidebar.jsx
+++ urservice/src/components/Sidebar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return <Container>Sidebar</Container>;
 };
 
 export default Sidebar;
