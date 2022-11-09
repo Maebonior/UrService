96d684922a115a34ece31d91d64c5b0c0e1959fb
Marlon Gacrama Jr <mgacrama_ccs@uspf.edu.ph>
2022-11-09 22:23:24 +0800

    Updated Route

--- urservice/src/App.js
+++ urservice/src/App.js
@@ -4,26 +4,22 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Plumbers from "./pages/Plumbers";
 // import Login from "./pages/Login";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Switch from "./components/Sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <div>
         <Navbar />
         <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/Plumbers">
-            <Plumbers />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Plumbers" element={<Plumbers />} />
+        </Routes>
+
         {/* <Login /> */}
       </div>
-    </Router>
+    </BrowserRouter>
   );
 };
 
